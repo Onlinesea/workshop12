a2b81795ad2bdb83ff67fcd35635a580d2a07205
Onlinesea <69972150+Onlinesea@users.noreply.github.com>
2022-07-13 17:37:42 +0800

    make it better

---
+++ Workshop12/src/main/java/Numbers/Workshop12/exeception/RandomNumberException.java
@@ -0,0 +1,5 @@
+package Numbers.Workshop12.exeception;
+
+public class RandomNumberException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+}
---
+++ Workshop12/src/main/java/Numbers/Workshop12/model/Generate.java
@@ -0,0 +1,14 @@
+package Numbers.Workshop12.model;
+
+
+public class Generate {
+    public int numberVal;
+
+    public int getNumberVal() {
+        return numberVal;
+    }
+
+    public void setNumberVal(int numberVal) {
+        this.numberVal = numberVal;
+    }
+}
